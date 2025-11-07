84f4936a0e25357f0d6371b720ff3a3d74d08bd6
Lukas Neef <lukas.neef@tum.de>
2025-11-07 12:58:08 +0100

    add ena com log

--- bsd/sys/contrib/ena_comv1/ena_plat.h
+++ bsd/sys/contrib/ena_comv1/ena_plat.h
@@ -75,7 +75,9 @@ extern struct ena_bus_space ebs;
 		(type *)((uintptr_t)__p - offsetof(type, member));	\
 	})
 
-#define ena_trace(ctx, level, fmt, args...) do {} while (0)
+#define ena_trace(ctx, level, fmt, args...)			\
+	ena_log((ctx)->dmadev, level, "%s() [TID:%d]: "		\
+	    fmt, __func__, curthread->td_tid, ##args)
 
 #define ena_trc_dbg(ctx, format, arg...)	\
 	ena_trace(ctx, DBG, format, ##arg)
