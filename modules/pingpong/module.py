77c1d6106a618d31f6791653b38015b1dfa7bbfa
Lukas Neef <lukas.neef@tum.de>
2025-11-07 11:33:50 +0100

    module.py

--- modules/pingpong/module.py
+++ modules/pingpong/module.py
@@ -1,7 +1,5 @@
 from osv.modules.api import *
 from osv.modules.filemap import FileMap
 from osv.modules import api
-usr_files = FileMap()
-usr_files.add('${OSV_BASE}/modules/pingpong').to('/pong')
-full = api.run('/pong')
+full = api.run('/pong.so')
 default = full
